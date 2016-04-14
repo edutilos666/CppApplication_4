887f3b5bfb8598d4a8c7aade9f34163f5d2ffeeb
edutilos <edutilosaghayev@yahoo.com>
2016-04-14 19:38:26 +0200

    s

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: edutilos
+ *
+ * Created on 14 April 2016, 19:37
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
